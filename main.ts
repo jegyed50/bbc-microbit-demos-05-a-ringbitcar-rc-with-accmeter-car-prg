911f981b0b341e433953353239a98119da598257
Egyed József <jegyed50@gmail.com>
2023-05-23 16:47:48 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "x") {
         xValue = value
+        if (xValue > -5 && xValue < 5) {
+            xValue = 0
+        }
     } else {
         if (name == "y") {
             yValue = value
+            if (yValue > -5 && yValue < 5) {
+                yValue = 0
+            }
         }
     }
 })
@@ -11,7 +17,7 @@ let rightwheel = 0
 let leftwheel = 0
 let yValue = 0
 let xValue = 0
-RingbitCar.init_wheel(AnalogPin.P2, AnalogPin.P1)
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P0)
 basic.showIcon(IconNames.Triangle)
 radio.setGroup(90)
 xValue = 0
